583b498a4a21e73599617cdf52c5026953e655f2
Emily Aquin <eaquin1@gmail.com>
2020-04-02 17:57:55 -0500

    added thanks template and route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
@@ -16,17 +16,28 @@ def starter_page():
     instructions = satisfaction_survey.instructions
     return render_template('title.html', survey_title=survey_title, instructions=instructions)
 
-@app.route('/question/<int:q_id>')
-def begin(q_id):
-    """Render question page"""
-    q = satisfaction_survey.questions[q_id]
-    question = q.question
-    choices = q.choices
-    return render_template('question.html', question=question, choices=choices)
-
 @app.route('/answer', methods=["POST"])
 def add_answer():
     answer = request.form["choice"]
-    print(answer)
     responses.append(answer)
-    return redirect(f"/question/{len(responses)}")
+    print(responses)
+    if len(responses) == len(satisfaction_survey.questions):
+        return redirect("/thanks")
+    else:
+        return redirect(f"/question/{len(responses)}")
+
+@app.route('/question/<int:q_id>')
+def begin(q_id):
+    """Render question page"""
+
+    if (len(responses) != q_id):
+        return redirect(f"/question/{len(responses)}")
+
+    q = satisfaction_survey.questions[q_id]
+    question = q.question
+    choices = q.choices
+    return render_template('question.html', question_num=q_id, question=question, choices=choices)
+
+@app.route('/thanks')
+def thanks():
+    return render_template('thanks.html')
